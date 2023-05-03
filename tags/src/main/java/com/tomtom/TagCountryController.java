cc5f3cf6ec255f7f87eb20466863dc277cd2deb1
Rajesh Patel <rajesh.patel@tomtom.com>
2023-05-03 15:39:45 +0530

    Added openAPi.

--- tags/src/main/java/com/tomtom/TagCountryController.java
+++ tags/src/main/java/com/tomtom/TagCountryController.java
@@ -1,20 +1,21 @@
 package com.tomtom;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
 @RequestMapping("v1")
 @Slf4j
+@AllArgsConstructor
 public class TagCountryController {
 
-    private TagCountryService tagCountryService;
+    private final TagCountryService tagCountryService;
 
     @PostMapping("tags/country/{country}")
     public Set<String> getTagsFromCountry(@PathVariable String country) {
--- tags/src/main/java/com/tomtom/TagsApplication.java
+++ tags/src/main/java/com/tomtom/TagsApplication.java
@@ -1,19 +1,27 @@
 package com.tomtom;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-import java.util.Set;
 
 @SpringBootApplication
-
 public class TagsApplication {
 
-	public static void main(String[] args) {
-		final ConfigurableApplicationContext run = SpringApplication.run(TagsApplication.class, args);
-		final TagCountryService tagCountryService = run.getBean(TagCountryService.class);
-		final Set<String> tagsFromCountry = tagCountryService.getTagsFromCountry("GBR");
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TagsApplication.class, args);
+    }
+
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI().info(
+                new Info().title("Tags").description("REST-endpoints")
+                        .contact((new Contact().email("orbis_search_sdp_alpha@groups.tomtom.com").name("Alpha")))
+        );
+    }
 
 }
